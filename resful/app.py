b1a67ee37e9f213438d0ccb2d7b743f490415cf0
Manuel Šarić <manujelko@gmail.com>
2022-08-06 16:59:05 +0100

    Item list and creating items

--- resful/app.py
+++ resful/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -13,14 +13,21 @@ class Item(Resource):
         for item in items:
             if item["name"] == name:
                 return item
-        return {'item': None}, 404
+        return {"item": None}, 404
 
     def post(self, name):
-        item = {"name": name, "price": 12}
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
 
 
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
+
+
 api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 
 app.run(port=5000)
