54b9c2e263f86628d2786343d90c7c702ee5c2d9
Manuel Šarić <manujelko@gmail.com>
2022-08-06 14:37:15 +0100

    Return a list of stores

--- first-flask/app.py
+++ first-flask/app.py
@@ -1,10 +1,36 @@
 from flask import Flask
+from flask.json import jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World"
+stores = [
+    {"name": "My Wonderful Store", "items": [{"name": "My Item", "price": 15.99}]}
+]
+
+
+@app.route("/store", methods=["POST"])
+def create_store():
+    pass
+
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    pass
+
+
+@app.route("/store")
+def get_stores():
+    return jsonify({"stores": stores})
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store(name):
+    pass
+
+
+@app.route("/store/<string:name>/item")
+def get_items_in_store(name):
+    pass
 
 
 app.run(port=5000)
