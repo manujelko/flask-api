837a7609768a7175b422830fde6019e7f00670a6
Manuel Šarić <manujelko@gmail.com>
2022-08-06 14:54:23 +0100

    Call the API from JavaScript

--- first-flask/app.py
+++ first-flask/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 stores = [
     {"name": "My Wonderful Store", "items": [{"name": "My Item", "price": 15.99}]}
 ]
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 @app.route("/store", methods=["POST"])
