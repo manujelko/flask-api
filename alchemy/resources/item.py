cc25b1fbd1c569abdbea5a342290a807904ae6e8
Manuel Šarić <manujelko@gmail.com>
2022-08-10 11:11:08 +0200

    Creating User and Item models

--- alchemy/resources/item.py
+++ alchemy/resources/item.py
@@ -2,6 +2,7 @@ import sqlite3
 
 from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
+from models.item import ItemModel
 
 
 class Item(Resource):
@@ -15,47 +16,23 @@ class Item(Resource):
     def get(self, name):
         item = self.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {"message": "Item not found"}, 404
 
-    @staticmethod
-    def find_by_name(name):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {"item": row[0], "price": row[1]}
-
     def post(self, name):
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {"message": f"An item with name '{name}' already exists"}, 400
 
         data = Item.parser.parse_args()
 
-        item = {"name": name, "price": data["price"]}
+        item = ItemModel(name, data["price"])
 
         try:
-            self.insert(item)
+            item.insert()
         except:
             return {"message": "An error occurred inserting the item."}, 500
 
-        return item, 201
-
-    @staticmethod
-    def insert(item):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item["name"], item["price"]))
-
-        connection.commit()
-        connection.close()
+        return item.json(), 201
 
     def delete(self, name):
         connection = sqlite3.connect("data.db")
@@ -72,32 +49,21 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
-        updated_item = {"name": name, "price": data["price"]}
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, data["price"])
 
         if not item:
             try:
-                self.insert(updated_item)
+                updated_item.insert()
             except:
                 return {"message": "An error occurred inserting the item."}, 500
         else:
             try:
-                self.update(updated_item)
+                updated_item.update()
             except:
                 return {"message": "An error occurred updating the item."}, 500
 
-        return updated_item
-
-    @staticmethod
-    def update(item):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=?, name=?"
-        cursor.execute(query, (item["price"], item["name"]))
-
-        connection.commit()
-        connection.close()
+        return updated_item.json()
 
 
 class ItemList(Resource):
