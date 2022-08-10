5f03c75dea4fced6f443b8a55a1c7d7a7e6ff4b2
Manuel Šarić <manujelko@gmail.com>
2022-08-10 10:07:39 +0200

    Writing our Item resources to a database

--- sql/item.py
+++ sql/item.py
@@ -13,6 +13,13 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 404
+
+    @staticmethod
+    def find_by_name(name):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -23,15 +30,24 @@ class Item(Resource):
 
         if row:
             return {"item": row[0], "price": row[1]}
-        return {"message": "Item not found"}, 404
 
     def post(self, name):
-        if next((item for item in items if item["name"] == name), None):
-            return {"message": f"An item with name '{name}' already exists."}, 400
+        if self.find_by_name(name):
+            return {"message": f"An item with name '{name}' already exists"}, 400
 
         data = Item.parser.parse_args()
+
         item = {"name": name, "price": data["price"]}
-        items.append(item)
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item["name"], item["price"]))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     def delete(self, name):
