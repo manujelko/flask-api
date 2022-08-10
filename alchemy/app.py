1c04a7d3198744b7b2a359c3a14a9aa2b13f08e8
Manuel Šarić <manujelko@gmail.com>
2022-08-10 13:01:04 +0200

    Creating the store resource

--- alchemy/app.py
+++ alchemy/app.py
@@ -5,12 +5,14 @@ from flask_restful import Api
 from resources.item import Item, ItemList
 from security import authenticate, identity
 from resources.user import UserRegister
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "jose"
 api = Api(app)
+
 
 @app.before_first_request
 def create_tables():
@@ -23,6 +25,8 @@ jwt = JWT(app, authenticate, identity)
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
 
 
 if __name__ == "__main__":
